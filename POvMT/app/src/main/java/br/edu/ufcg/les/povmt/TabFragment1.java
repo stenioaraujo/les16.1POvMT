602f0595c849daf438297f9822772207f79b4f07
Luiz Henrique <luizha.cc@gmail.com>
2016-07-16 20:25:02 -0300

    Tela inicial

--- POvMT/app/src/main/java/br/edu/ufcg/les/povmt/TabFragment1.java
+++ POvMT/app/src/main/java/br/edu/ufcg/les/povmt/TabFragment1.java
@@ -21,6 +21,7 @@ public class TabFragment1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.tab_fragment_1, container, false);
+
     }
 
 
